9ad614cf3e7960c04d55e108b3df772f295566d3
Fedelli98 <50577589+Fedelli98@users.noreply.github.com>
2023-06-11 20:48:38 +0200

    trusted origin added

--- PPMecommerce/settings.py
+++ PPMecommerce/settings.py
@@ -134,3 +134,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'https://web-production-efeb.up.railway.app',
+]
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-efeb.up.railway.app']
